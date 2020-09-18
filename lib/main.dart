5b425200e6b0e165cfb46c35d10674dc776c884a
Mehul Sudrik <57554158+mehulsudrik2310@users.noreply.github.com>
2020-09-18 13:20:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(OSProject());
@@ -91,11 +92,120 @@ class _AlgorithmPageState extends State<AlgorithmPage> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-        backgroundColor: Colors.lightGreenAccent,
-        appBar: AppBar(
-          title: Text('Algo xyz',style: TextStyle(color: Colors.white),),
-          centerTitle: true,
+      backgroundColor: Colors.lightGreenAccent,
+      appBar: AppBar(
+        title: Text('Algo xyz',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: GetTextFieldValue(),
+      ),
+    );
+  }
+}
+
+class GetTextFieldValue extends StatefulWidget {
+
+  _GetTextFieldValueState createState() => _GetTextFieldValueState();
+
+}
+
+class _GetTextFieldValueState extends State<GetTextFieldValue> {
+
+  final pagesEntryTextBox = TextEditingController();
+  final pageCapacityTextBox = TextEditingController();
+  String input_pages = '';
+  int frame_capacity = 0;
+  int i=0;
+  List<int> pages_arr = new List();
+  void getCapacity() {
+    setState(() {
+      frame_capacity = int.parse(pageCapacityTextBox.text);
+    });
+  }
+  void getTextInputData() {
+    setState(() {
+      pages_arr.add(int.parse(pagesEntryTextBox.text));
+      pagesEntryTextBox.text = '';
+    });
+  }
+  void clear_all() {
+    setState(() {
+      pagesEntryTextBox.text = '';
+      pageCapacityTextBox.text = '';
+      pages_arr.clear();
+      frame_capacity = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 190,
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: pageCapacityTextBox,
+                    autocorrect: true,
+                    decoration: InputDecoration(hintText: 'Enter the frame size'),
+                  )
+                ),
+                RaisedButton(
+                  onPressed: getCapacity,
+                  color: Colors.redAccent,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Text('Enter',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child : Text("Page capacity = $frame_capacity", style: TextStyle(fontSize: 20))
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 280,
+                  padding: EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: pagesEntryTextBox,
+                    autocorrect: true,
+                    decoration: InputDecoration(hintText: 'Enter the series of pages one by one'),
+                  )
+                ),
+                RaisedButton(
+                  onPressed: getTextInputData,
+                  color: Colors.redAccent,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Text('Enter',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child : Text("Entered pages are = $pages_arr", style: TextStyle(fontSize: 20))
+            ),
+            RaisedButton(
+              onPressed: clear_all,
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Text('Clear',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            ),
+          ],
         ),
+      )
     );
   }
 }
